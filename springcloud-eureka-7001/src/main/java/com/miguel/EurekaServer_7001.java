4e36792151129b35e0cf8b94a3b510a403fc94b3
zx_panjiaming <595156223@qq.com>
2021-02-24 13:25:41 +0800

    spring cloud: register provider client

--- springcloud-eureka-7001/src/main/java/com/miguel/EurekaServer_7001.java
+++ springcloud-eureka-7001/src/main/java/com/miguel/EurekaServer_7001.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaServer //启用Eureka服务端
 public class EurekaServer_7001 {
 
     public static void main(String[] args) {
