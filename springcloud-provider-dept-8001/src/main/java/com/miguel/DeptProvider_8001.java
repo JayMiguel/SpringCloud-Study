261ed1c4e8946551a804521a7fbe4a0ee260ed3a
zx_panjiaming <595156223@qq.com>
2021-02-24 16:18:32 +0800

    spring cloud: create register center colony

--- springcloud-provider-dept-8001/src/main/java/com/miguel/DeptProvider_8001.java
+++ springcloud-provider-dept-8001/src/main/java/com/miguel/DeptProvider_8001.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient //启用Eureka客户端
-@EnableDiscoveryClient //开启服务发现
+@EnableEurekaClient //向服务中心注册该服务，该注解只适用于Eureka
+// @EnableDiscoveryClient //作用同上，该注解适用于其他注册中心
 public class DeptProvider_8001 {
 
     public static void main(String[] args) {
