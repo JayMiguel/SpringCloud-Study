ac11cb87315e8c88e119b9d63d6dd510a58dcc75
zx_panjiaming <595156223@qq.com>
2021-02-24 15:38:09 +0800

    spring cloud: create register center colony

---
+++ springcloud-eureka-7003/src/main/java/com/miguel/EurekaServer_7003.java
@@ -0,0 +1,14 @@
+package com.miguel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer //启用Eureka服务端
+public class EurekaServer_7003 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer_7003.class, args);
+    }
+}
